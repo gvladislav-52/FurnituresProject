3f8ebd4234eed6474c9ccc7cf4b18fef5d77ade9
gvladislav-52 <gvladislav_52@mail.ru>
2025-04-17 10:37:06 +0300

    Initial Commit

---
+++ MyFurnitureAppUI/MyFurnitureAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFurnitureAppUIApp.swift
+//  MyFurnitureAppUI
+//
+//  Created by Senla on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFurnitureAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
